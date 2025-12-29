1efcea5bd681cc0a9a070e25f1797dc47d15fb6b
iamsondev <sondipkumarsk@gmail.com>
2025-12-29 13:05:01 +0600

    update and delete user by using prisma

--- crud.ts
+++ crud.ts
@@ -9,7 +9,6 @@ async function run() {
   //   },
   // });
   // console.log("createdUser", createUser);
-
   // create post
   // const createPost = await prisma.post.create({
   //   data: {
@@ -18,15 +17,71 @@ async function run() {
   //   },
   // });
   // console.log("postCreated", createPost);
-
   // create profile
-  const createProfile = await prisma.profile.create({
-    data: {
-      bio: "Full stack developer",
-      userId: 2,
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "Full stack developer",
+  //     userId: 2,
+  //   },
+  // });
+  // console.log("profileCreated", createProfile);
+  // retrieved all user
+  // const users = await prisma.user.findMany({
+  //   // include: {
+  //   //   posts: true,
+  //   //   profile: true,
+  //   // },
+  //   select: {
+  //     id: true,
+  //     name: true,
+  //     email: true,
+  //     posts: true,
+  //     profile: true,
+  //   },
+  // });
+  // console.dir(users, { depth: Infinity });
+  // update user
+  // const updateUser = await prisma.profile.update({
+  //   where: {
+  //     userId: 2,
+  //   },
+  //   data: {
+  //     bio: "Full stack developer at BMW",
+  //     dateOfBirth: "2025-12-28T17:44:40.549Z",
+  //   },
+  //   select: {
+  //     id: true,
+  //     bio: true,
+  //     user: {
+  //       select: {
+  //         name: true,
+  //         email: true,
+  //       },
+  //     },
+  //   },
+  // });
+  // console.log("update data", updateUser);
+
+  // delete User
+  // const deleteUser = await prisma.user.delete({
+  //   where: {
+  //     id: 1,
+  //   },
+  // });
+  // console.log("deleted user", deleteUser);
+
+  // retrieved specific id data
+
+  const getUserById = await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      posts: true,
+      profile: true,
     },
   });
-  console.log("profileCreated", createProfile);
+  console.log("specific id", getUserById);
 }
 
 run();
